24547d680b374ead0f9fcc28f4ccb342fde2c8b9
Tatiana <Bowmantatiana2@gmail.com>
2023-02-02 15:50:55 -0800

    commit tree fe4c1c16fcf392a99ab6c0135fe4c94a9709e7c4

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 20, "M", "Brown", "Warm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 30, "F", "Black", "Hot", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 10, "F", "White", "Cold", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeant Brown", 11, "M", "Brown", "Warm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 34, "F", "White", "Cold", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 35, "M", "Black", "Warm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 14, "F", "Brown", "Warm", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 21, "F", "Black", "Hot", 1);
+
+
